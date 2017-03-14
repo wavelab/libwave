cb5dad2f6fa1a95f6e18ba88fc5161e489d208c0
Chris Choi <chutsu@gmail.com>
2017-03-14 13:01:44 -0400

    Refactor travis sscripts

---
+++ scripts/ci_run.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e  # exit on first error
+
+# compile libwave
+mkdir -p build
+cd build
+cmake ..
+make
+
+# run tests
+cd wave_utils && ./tests/wave_utils_tests && cd -
--- scripts/install/install_deps.bash
+++ scripts/install/install_deps.bash
@@ -2,6 +2,9 @@
 set -e  # exit on first error
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+sudo apt-get clean
+sudo apt-get update
+
 sudo bash $SCRIPT_PATH/boost_install.bash
 # sudo bash $SCRIPT_PATH/ceres_install.bash
 sudo bash $SCRIPT_PATH/eigen_install.bash
