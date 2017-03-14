1fceab9f2df501dccd0ccdf7a453f9d07646c856
Chris Choi <chutsu@gmail.com>
2017-03-13 22:47:14 -0400

    This is getting old

--- scripts/install/install_deps.bash
+++ scripts/install/install_deps.bash
@@ -2,8 +2,8 @@
 set -e  # exit on first error
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-sudo sh $SCRIPT_PATH/boost_install.bash
-# sudo sh $SCRIPT_PATH/ceres_install.bash
-sudo sh $SCRIPT_PATH/eigen_install.bash
-sudo sh $SCRIPT_PATH/yaml_cpp_install.bash
-sudo sh $SCRIPT_PATH/opencv3_install.bash
+sudo bash $SCRIPT_PATH/boost_install.bash
+# sudo bash $SCRIPT_PATH/ceres_install.bash
+sudo bash $SCRIPT_PATH/eigen_install.bash
+sudo bash $SCRIPT_PATH/yaml_cpp_install.bash
+sudo bash $SCRIPT_PATH/opencv3_install.bash
