d0fbc4a8d203d37c828cc3a92d53855272e9c57c
Ben <benjamin.skikos@gmail.com>
2017-03-17 12:24:03 -0400

    Add large pointcloud test

--- wave_matching/config/fast_config.lua
+++ wave_matching/config/fast_config.lua
@@ -1,11 +1,11 @@
 
 FAST_CONFIG = {
-    branch_and_bound_depth = 5,
-    full_resolution_depth = 5,
+    branch_and_bound_depth = 8,
+    full_resolution_depth = 8,
     rotational_histogram_size = 30,
     min_rotational_score = 0.1,
-    linear_xy_search_window = 0.8,
-    linear_z_search_window = 0.8,
+    linear_xy_search_window = 0.5,
+    linear_z_search_window = 0.2,
     angular_search_window = 0.3,
     hit_probability = 0.8,
     miss_probability = 0.1,
--- wave_matching/tests/fast_corr_tests.cpp
+++ wave_matching/tests/fast_corr_tests.cpp
@@ -74,6 +74,32 @@ TEST(fast_corr_tests, SmallDisplacement) {
     EXPECT_TRUE(isApprox(result, perturb, 0.1));
 }
 
+TEST(fast_corr_tests, LargePointCloud) {
+    //Setup
+    wave::matching::FastCorrelationMatcher matcher(0.1f);
+    cartographer::sensor::PointCloud point_cloud, dupe;
+    // load pointclound from pcd file. Pointcloud is from HDL32
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_point_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::io::loadPCDFile("../tests/testscan.pcd", *pcl_point_cloud);
+    for (const auto& point : *pcl_point_cloud) {
+        point_cloud.emplace_back(point.x, point.y, point.z);
+    }
+
+    Eigen::Affine3d perturb = Eigen::Affine3d::Identity();
+    perturb.translation() << 0.3, 0, 0;
+    Eigen::Matrix3f mat = perturb.rotation().cast<float> ();
+    Eigen::Quaternionf q(mat);
+    cartographer::transform::Rigid3f perturb_c(perturb.translation().cast<float>(), q);
+    dupe = cartographer::sensor::TransformPointCloud(point_cloud, perturb_c);
+    matcher.setup(point_cloud, dupe);
+    //Run Matcher
+    EXPECT_TRUE(matcher.match());
+    Eigen::Affine3d result = matcher.get_result();
+    perturb = perturb.inverse();
+    // Check Result
+    EXPECT_TRUE(isApprox(result, perturb, 0.1));
+}
+
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
