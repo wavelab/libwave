be199fc80e75937002fd2e90e2631929db832e2c
Chris Choi <chutsu@gmail.com>
2017-04-06 16:48:27 -0400

    Clean up cmake files

--- scripts/ci_run.bash
+++ scripts/ci_run.bash
@@ -6,7 +6,6 @@ CMAKE_CONFIG_DIR="../cmake"  # cmake folder relative to this script
 compile_libwave() {
     mkdir -p build
     cd build
-    export Eigen3_DIR="/usr/include/eigen3:$Eigen3_DIR"
     cmake .. -DCMAKE_MODULE_PATH=$DIR/$CMAKE_CONFIG_DIR
     make
 }
