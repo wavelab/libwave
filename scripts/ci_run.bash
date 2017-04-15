7323998503ec70e67749fe76cca4d4ffc0355328
Arun Das <arun.das.uwaterloo@gmail.com>
2017-04-15 16:43:41 -0400

    Feature 12 rotation kinematics (#24)
    * Add initial code for rotations library.
    * Add documentation for rotation class.
    * Fixe minor spelling errors in docs.
    * Add kindr as part of install scripts

--- scripts/ci_run.bash
+++ scripts/ci_run.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e  # exit on first error
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd)"
+CMAKE_CONFIG_DIR="../cmake"  # cmake folder relative to this script
 
 compile_libwave() {
     mkdir -p build
     cd build
-    cmake ..
+    cmake .. -DCMAKE_MODULE_PATH=$DIR/$CMAKE_CONFIG_DIR
     make
 }
 
--- scripts/install/install_deps.bash
+++ scripts/install/install_deps.bash
@@ -3,6 +3,7 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 sudo apt-get clean > /dev/null
 sudo apt-get update > /dev/null
+sudo apt-get install cmake g++ > /dev/null
 
 install_dependency() {
     echo "Installing $1 ..."
@@ -20,8 +21,9 @@ install_dependency() {
 }
 
 install_dependency "Boost" $SCRIPT_PATH/boost_install.bash
+install_dependency "Yaml-CPP" $SCRIPT_PATH/yaml_cpp_install.bash
 install_dependency "Ceres" $SCRIPT_PATH/ceres_install.bash
 install_dependency "Eigen" $SCRIPT_PATH/eigen_install.bash
-install_dependency "Yaml-CPP" $SCRIPT_PATH/yaml_cpp_install.bash
+install_dependency "Kindr" $SCRIPT_PATH/kindr_install.bash
 install_dependency "OpenCV 2.4" $SCRIPT_PATH/opencv2_install.bash
 install_dependency "PCL 1.7.2" $SCRIPT_PATH/pcl_install.bash
