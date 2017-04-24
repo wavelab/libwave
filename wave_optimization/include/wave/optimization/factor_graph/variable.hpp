87714d45eafe29a106da2562abbe8d9b6fb2abdf
Chris Choi <chutsu@gmail.com>
2017-04-24 14:27:36 -0400

    Remove vision module

--- wave_optimization/include/wave/optimization/factor_graph/variable.hpp
+++ wave_optimization/include/wave/optimization/factor_graph/variable.hpp
@@ -1,14 +1,17 @@
 #ifndef WAVE_OPTIMIZATION_FACTOR_GRAPH_VARIABLE_HPP
 #define WAVE_OPTIMIZATION_FACTOR_GRAPH_VARIABLE_HPP
 
+#include "wave/utils/utils.hpp"
+
 namespace wave {
 
 struct Variable {
     size_t id;
     size_t size;
+    VecX data;
 
-    Variable() : id{0}, size{0} {}
-    Variable(size_t id, size_t size) : id{id}, size{size} {}
+    Variable() : id{0}, size{0}, data{VecX(0)} {}
+    Variable(size_t id, size_t size) : id{id}, size{size}, data{VecX(size)} {}
 
     std::string toString() {
         std::ostringstream oss;
@@ -30,6 +33,10 @@ struct Variable {
             return true;
         }
         return false;
+    }
+
+    VecX &getData() {
+        return data;
     }
 };
 
