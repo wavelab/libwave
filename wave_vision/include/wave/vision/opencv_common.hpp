6b8dac5b96e99f990df7ce2b406e3ef70143fe3f
Nav Ganti <navganti.12@gmail.com>
2017-05-13 13:48:26 -0400

    Initial commit. Incorporated wave_vision directory, modified CMakeLists.txt

---
+++ wave_vision/include/wave/vision/opencv_common.hpp
@@ -0,0 +1,12 @@
+#ifndef WAVE_OPENCV_COMMON_HPP
+#define WAVE_OPENCV_COMMON_HPP
+
+#include <opencv2/opencv.hpp>
+
+namespace wave {
+
+typedef cv::Mat OpenCVImage;
+
+} // end of namespace wave
+
+#endif //WAVE_OPENCV_COMMON_HPP
