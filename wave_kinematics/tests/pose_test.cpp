ef0132697f7777cdfa6fbea59d11c32c1b9bb12e
Chris Choi <chutsu@gmail.com>
2017-05-15 18:28:38 -0400

    Refactor pose tests

--- wave_kinematics/tests/pose_test.cpp
+++ wave_kinematics/tests/pose_test.cpp
@@ -6,38 +6,20 @@ namespace wave {
 TEST(Pose, constructor) {
     // test default constructor
     Pose p1;
-
-    EXPECT_EQ(0.0, p1.position(0));
-    EXPECT_EQ(0.0, p1.position(1));
-    EXPECT_EQ(0.0, p1.position(2));
-    EXPECT_EQ(1.0, p1.orientation.w());
-    EXPECT_EQ(0.0, p1.orientation.x());
-    EXPECT_EQ(0.0, p1.orientation.y());
-    EXPECT_EQ(0.0, p1.orientation.z());
+    EXPECT_TRUE(p1.position.isApprox(Vec3{0.0, 0.0, 0.0}));
+    EXPECT_TRUE(p1.orientation.isApprox(Quaternion{1.0, 0.0, 0.0, 0.0}));
 
     // test quaternion + position constructor
     Quaternion orientation{1.0, 2.0, 3.0, 4.0};
     Vec3 position{1.0, 2.0, 3.0};
     Pose p2{position, orientation};
-
-    EXPECT_EQ(1.0, p2.position(0));
-    EXPECT_EQ(2.0, p2.position(1));
-    EXPECT_EQ(3.0, p2.position(2));
-    EXPECT_EQ(1.0, p2.orientation.w());
-    EXPECT_EQ(2.0, p2.orientation.x());
-    EXPECT_EQ(3.0, p2.orientation.y());
-    EXPECT_EQ(4.0, p2.orientation.z());
+    EXPECT_TRUE(p2.position.isApprox(Vec3{1.0, 2.0, 3.0}));
+    EXPECT_TRUE(p2.orientation.isApprox(Quaternion{1.0, 2.0, 3.0, 4.0}));
 
     // test euler + position constructor
     Pose p3(0.0, 0.0, 0.0, 4.0, 5.0, 6.0);
-
-    EXPECT_EQ(4.0, p3.position(0));
-    EXPECT_EQ(5.0, p3.position(1));
-    EXPECT_EQ(6.0, p3.position(2));
-    EXPECT_EQ(1.0, p3.orientation.w());
-    EXPECT_EQ(0.0, p3.orientation.x());
-    EXPECT_EQ(0.0, p3.orientation.y());
-    EXPECT_EQ(0.0, p3.orientation.z());
+    EXPECT_TRUE(p3.position.isApprox(Vec3{4.0, 5.0, 6.0}));
+    EXPECT_TRUE(p3.orientation.isApprox(Quaternion{1.0, 0.0, 0.0, 0.0}));
 }
 
 TEST(Pose, rotationMatrix) {
