a74768f2dc69f65d55d59207c69ec62e7abf394a
Leo Koppel <lkoppel@uwaterloo.ca>
2017-05-18 17:14:43 -0400

    Add option to disable a test
    Add a DISABLED option to wave_add_test, which excludes that test from "make
    test".
    
    Needed to maintain current behaviour of building but not running
    wave_matching_tests.
    
    Also add documentation to the function.

--- cmake/WaveUtils.cmake
+++ cmake/WaveUtils.cmake
@@ -1,11 +1,20 @@
+# - Macros used in libwave
+#
+# wave_add_test: Add a gtest target
+#
+# WAVE_ADD_TEST(Name [DISABLED] src1 [src2...])
+#
+# The test will be added to the tests run by "make test", unless DISABLED is
+# given. It will be linked against the needed gtest libraries. Any other links
+# can be made separately with the target_link_libraries command.
 function(wave_add_test NAME)
-    set(options "")
+
+    # Define the arguments this function accepts
+    set(options DISABLED)
     set(one_value_args "")
     set(multi_value_args "")
     cmake_parse_arguments(WAVE_ADD_TEST
         "${options}" "${one_value_args}" "${multi_value_args}" ${ARGN})
-
-    message(STATUS "Adding test ${NAME}")
 
     # Build the test executable using the given sources
     add_executable(${NAME} ${WAVE_ADD_TEST_UNPARSED_ARGUMENTS})
@@ -17,7 +26,12 @@ function(wave_add_test NAME)
     set_target_properties(${NAME} PROPERTIES
         RUNTIME_OUTPUT_DIRECTORY ${PROJECT_BINARY_DIR}/tests)
 
-    # Add the executable as a test, so it runs with "make test"
-    add_test(NAME ${NAME} COMMAND ${NAME})
+    if(WAVE_ADD_TEST_DISABLED)
+        message(WARNING "Test ${NAME} is disabled: building, but not adding it to tests")
+    else()
+        message(STATUS "Adding test ${NAME}")
+        # Add the executable as a test, so it runs with "make test"
+        add_test(NAME ${NAME} COMMAND ${NAME})
+    endif()
 
 endfunction(wave_add_test)
