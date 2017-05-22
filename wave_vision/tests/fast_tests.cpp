0191caf3e720b8cc78e50215690200396c221c8b
Nav Ganti <navganti.12@gmail.com>
2017-05-22 18:48:22 -0400

    Final tests written

--- wave_vision/tests/fast_tests.cpp
+++ wave_vision/tests/fast_tests.cpp
@@ -15,18 +15,22 @@ class FASTTest : public testing::Test {
             delete this->detector;
         }
     }
-    virtual void SetUp() {
-        this->image = cv::imread(TEST_IMAGE, cv::IMREAD_COLOR);
-    }
 
     void initDetector() {
         this->detector = new FASTDetector(TEST_CONFIG);
     }
 
+    virtual void SetUp() {
+        initDetector();
+        this->image = cv::imread(TEST_IMAGE, cv::IMREAD_COLOR);
+    }
+
     cv::Mat image;
     FASTDetector *detector;
+    std::vector<cv::KeyPoint> keypoints;
 };
 
+//
 TEST(FASTTests, GoodInitialization) {
     FASTDetector detector(TEST_CONFIG);
     SUCCEED();
@@ -37,6 +41,29 @@ TEST(FASTTests, BadInitialization) {
 
     ASSERT_THROW(FASTDetector detector(bad_path),
                  ConfigurationLoadingException);
+}
+
+TEST(FASTTests, GoodManualConfiguration) {
+    FASTDetector detector(TEST_CONFIG);
+    int threshold = 10;
+    bool nonmax_suppression = true;
+    int type = 2;
+
+    int check_threshold;
+    bool check_nonmax_suppression;
+    int check_type;
+
+    // Configure detector with valid values
+    detector.configure(threshold, nonmax_suppression, type);
+
+    // Extract configuration from detector, assert values have been properly set
+    detector.getConfiguration(check_threshold,
+                              check_nonmax_suppression,
+                              check_type);
+
+    ASSERT_EQ(threshold, check_threshold);
+    ASSERT_EQ(nonmax_suppression, check_nonmax_suppression);
+    ASSERT_EQ(type, check_type);
 }
 
 TEST(FASTTests, BadThresholdConfiguration) {
@@ -61,6 +88,20 @@ TEST(FASTTests, BadTypeConfiguration) {
 
     ASSERT_THROW(detector.configure(threshold, nonmax_suppression, bad_type_pos),
                  InvalidConfigurationException);
+}
+
+TEST_F(FASTTest, DetectImage) {
+    this->keypoints = detector->detectFeatures(this->image);
+    cv::imshow("Lenna", this->image);
+
+    // Draw Keypoints on image and display
+    cv::Mat image_with_keypoints;
+    cv::drawKeypoints(this->image, this->keypoints, image_with_keypoints,
+                      cv::Scalar::all(-1), cv::DrawMatchesFlags::DEFAULT);
+    cv::imshow("Detection Test", image_with_keypoints);
+
+    //cv::waitKey(0);
+    SUCCEED();
 }
 
 }  // end of namespace wave
