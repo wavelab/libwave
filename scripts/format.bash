112adea99483b9e69f06c27eb5058053025e2d93
Leo Koppel <lkoppel@uwaterloo.ca>
2017-05-25 17:50:03 -0400

    Add paths argument to format.bash

--- scripts/format.bash
+++ scripts/format.bash
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Usage: format.bash [path to search]
+# clang-format will be called for each .hpp and .cpp file in the given path(s)
+# The default is: ./wave_*/
+
+SEARCH_PATHS=${@:-./wave_*/}
+
 # This variable can be set to use a different version of clang-format
 : ${FORMAT_EXE:=clang-format}
 
 # Format all source files. Run from the source root directory.
-find ./wave_*/ -name "*.hpp" -o -name "*.cpp" | xargs ${FORMAT_EXE} -i
+find ${SEARCH_PATHS} -name "*.hpp" -o -name "*.cpp" | xargs ${FORMAT_EXE} -i
