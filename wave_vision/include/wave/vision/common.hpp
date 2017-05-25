1a244983cb6926b94a805e891d863098aa934b37
Chris Choi <chutsu@gmail.com>
2017-05-25 19:47:22 -0400

    Edit docs in vision common

--- wave_vision/include/wave/vision/common.hpp
+++ wave_vision/include/wave/vision/common.hpp
@@ -16,33 +16,25 @@
 
 namespace wave {
 
-/**
- * Calculate theoretical focal length based on field of view (fov) in radians
- * and image width in pixels
- */
+/** Calculate theoretical focal length based on field of view (fov) in radians
+ * and image width in pixels */
 double focal_length(double fov, double image_width);
 
-/**
- * Calculate theoretical focal length based on field of view (fov) in *
- * horizontal and vertical plane in radians and image width and height in pixels
- */
+/** Calculate theoretical focal length based on field of view (fov) in
+ * horizontal and vertical plane in radians and image width and height in
+ * pixels */
 Vec2 focal_length(double hfov,
                   double vfov,
                   double image_width,
                   double image_height);
 
-/**
- * Combine 2 matrices together to form a third, this function was design to
+/** Combine 2 matrices together to form a third, this function was design to
  * concatenate two image matrices `img1` and `img2` so they can form a third
- * side
- * by side image `out`
- */
+ * side by side image `out` */
 void matconcat(const cv::Mat &img1, const cv::Mat &img2, cv::Mat &out);
 
-/**
- * Create projection matrix `P` based on camera intrinsics `K`, camera rotation
- * `R` and camera position `t`
- */
+/** Create projection matrix `P` based on camera intrinsics `K`, camera
+ * rotation `R` and camera position `t` */
 void projection_matrix(const Mat3 &K, const Mat3 &R, const Vec3 &t, MatX &P);
 
 }  // end of wave namespace
