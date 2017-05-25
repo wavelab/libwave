b09ac41071a4b62fc96a58611f802f19bf3ac924
Leo Koppel <lkoppel@uwaterloo.ca>
2017-05-25 19:02:44 -0400

    Add comment to clarify default variable setting

--- scripts/format.bash
+++ scripts/format.bash
@@ -2,11 +2,11 @@
 
 # Usage: format.bash [path to search]
 # clang-format will be called for each .hpp and .cpp file in the given path(s)
-# The default is: ./wave_*/
-
+# The default is set below:
 SEARCH_PATHS=${@:-./wave_*/}
 
-# This variable can be set to use a different version of clang-format
+# Set the variable FORMAT_EXE to use a different version of clang-format
+# The default is set below:
 : ${FORMAT_EXE:=clang-format}
 
 # Format all source files. Run from the source root directory.
