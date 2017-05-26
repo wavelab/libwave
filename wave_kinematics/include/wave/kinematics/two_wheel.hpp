cbb6c3f6df0dd1aa4094c0f300acce0e73c74941
Chris Choi <chutsu@gmail.com>
2017-05-26 11:13:55 -0400

    Add docs to two wheel

--- wave_kinematics/include/wave/kinematics/two_wheel.hpp
+++ wave_kinematics/include/wave/kinematics/two_wheel.hpp
@@ -1,3 +1,7 @@
+/** @file
+ * @ingroup kinematics
+ */
+
 #ifndef WAVE_KINEMATICS_TWOWHEEL_HPP
 #define WAVE_KINEMATICS_TWOWHEEL_HPP
 
@@ -7,15 +11,25 @@
 #include "wave/utils/utils.hpp"
 
 namespace wave {
+/** @addtogroup kinematics
+ *  @{ */
 
+/** Generic two wheel robot motion model */
 class TwoWheelRobot2DModel {
  public:
     Vec3 pose;
 
     TwoWheelRobot2DModel() : pose{0.0, 0.0, 0.0} {}
     TwoWheelRobot2DModel(Vec3 pose) : pose{pose} {}
+
+    /** Update two wheel model
+     * @param inputs Model input vector where first input is wheel velocity in
+     * $ms^-1$ and the second input is steering angular velocity $rads^-1$
+     * @param dt Update time step in seconds
+     */
     Vec3 update(Vec2 inputs, float dt);
 };
 
+/** @} end of group */
 }  // end of wave namespace
 #endif
--- wave_kinematics/src/two_wheel.cpp
+++ wave_kinematics/src/two_wheel.cpp
@@ -5,11 +5,9 @@ namespace wave {
 Vec3 TwoWheelRobot2DModel::update(Vec2 inputs, float dt) {
     Vec3 g;
 
-    // clang-format off
-		g << this->pose(0) + inputs(0) * cos(this->pose(2)) * dt,
-				this->pose(1) + inputs(0) * sin(this->pose(2)) * dt,
-				this->pose(2) + inputs(1) * dt;
-    // clang-format on
+    g << this->pose(0) + inputs(0) * cos(this->pose(2)) * dt,  //
+      this->pose(1) + inputs(0) * sin(this->pose(2)) * dt,     //
+      this->pose(2) + inputs(1) * dt;
 
     return g;
 }
