d8bcf70e2a6c74e68ed4de0e5dcad2531f62a020
Chris Choi <chutsu@gmail.com>
2017-05-26 13:10:36 -0400

    Update test

--- wave_kinematics/tests/two_wheel_test.cpp
+++ wave_kinematics/tests/two_wheel_test.cpp
@@ -8,7 +8,6 @@ TEST(TwoWheelRobot2DModel, update) {
     TwoWheelRobot2DModel robot;
 
     // Motor inputs for circle trajectory:
-    //
     // Assuming we want to travel a 10 m radius circle at 1 ms^-1
     //
     // distance = 2 * pi * radius = 20 * pi
@@ -16,14 +15,16 @@ TEST(TwoWheelRobot2DModel, update) {
     // angular_velocity = (2 * pi) / time_taken = (2 * pi) / (20 * pi) = 0.1
     //
     // therefore motor inputs is [1.0, 0.1]
-    double v = 1.0;
-    double t = 0.0;
-    double t_end = 20 * M_PI;
-    double dt = 0.001;
-    double w = (2 * M_PI) / t_end;
-    Vec2 motor_inputs = Vec2{v, w};
+    double circle_radius = 10.0;
+    double distance = 2 * M_PI * circle_radius;
+    double velocity = 1.0;
+    double t_end = distance / velocity;
+    double angular_velocity = (2 * M_PI) / t_end;
+    Vec2 motor_inputs = Vec2{velocity, angular_velocity};
 
     // simulate and assert
+    double t = 0.0;
+    double dt = 0.001;
     while (t <= t_end) {
         robot.update(motor_inputs, dt);
         t += dt;
