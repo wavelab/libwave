ea934fffd3b97478b6fd5978bec6768525c3a623
Leo Koppel <lkoppel@uwaterloo.ca>
2017-05-27 16:54:03 -0400

    Remove extra includes

--- wave_containers/include/wave/containers/impl/measurement_container_base.hpp
+++ wave_containers/include/wave/containers/impl/measurement_container_base.hpp
@@ -1,8 +1,4 @@
-#include <boost/multi_index_container.hpp>
-#include <boost/multi_index/member.hpp>
-#include <boost/multi_index/ordered_index.hpp>
-#include <boost/multi_index/composite_key.hpp>
-#include <boost/version.hpp>
+#include <boost/tuple/tuple.hpp>
 
 namespace wave {
 
