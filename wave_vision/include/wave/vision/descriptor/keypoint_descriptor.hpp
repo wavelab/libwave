75c5a32b61006631511aaaf5c92d357376207b2e
Nav Ganti <navganti.12@gmail.com>
2017-05-27 17:29:39 -0400

    Initial commit for descriptors, and base class

--- wave_vision/include/wave/vision/descriptor/keypoint_descriptor.hpp
+++
@@ -1,9 +0,0 @@
-/**
- * @file
- * Base class from which keypoint descriptors can be derived.
- * @ingroup vision
- */
-#ifndef WAVE_KEYPOINT_DESCRIPTOR_HPP
-#define WAVE_KEYPOINT_DESCRIPTOR_HPP
-
-#endif  // WAVE_KEYPOINT_DESCRIPTOR_HPP
