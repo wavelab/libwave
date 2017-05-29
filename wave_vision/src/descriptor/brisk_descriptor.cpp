5227031136d218ed9d377fc27c71c3b8614b41fe
Nav Ganti <navganti.12@gmail.com>
2017-05-29 13:01:43 -0400

    Begin .cpp file, and add to CMakeLists

---
+++ wave_vision/src/descriptor/brisk_descriptor.cpp
@@ -0,0 +1,13 @@
+// Libwave Headers
+#include "wave/vision/descriptor/brisk_descriptor.hpp"
+
+namespace wave {
+
+// Default constructor
+BRISKDescriptor::BRISKDescriptor() {
+    // Instantiate cv::BRISK with default values
+}
+
+// Destructor
+BRISKDescriptor::~BRISKDescriptor() {}
+}  // End of namespace wave
