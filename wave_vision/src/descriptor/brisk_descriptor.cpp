b0e24cdc25bd3ca03da495000bd35c2a2710cb3b
Nav Ganti <navganti.12@gmail.com>
2017-05-30 00:53:48 -0400

    Development of second constructor and checkConfiguration function

--- wave_vision/src/descriptor/brisk_descriptor.cpp
+++ wave_vision/src/descriptor/brisk_descriptor.cpp
@@ -43,11 +43,64 @@ BRISKDescriptor::BRISKDescriptor() {
 BRISKDescriptor::BRISKDescriptor(const BRISKDescriptorParams &config) {
     // Ensure parameters are valid
     this->checkConfiguration(config);
+
+    // Create unused indexChange vector
+    const std::vector<int> indexChange = std::vector<int>();
+
+    // Create cv::BRISK object with the desired parameters
+    this->brisk_descriptor = cv::BRISK::create(config.radiusList,
+                                               config.numberList,
+                                               config.dMax,
+                                               config.dMin,
+                                               indexChange);
 }
 
 // Destructor
 BRISKDescriptor::~BRISKDescriptor() {}
 
 void BRISKDescriptor::checkConfiguration(
-  const BRISKDescriptorParams &check_config) {}
+  const BRISKDescriptorParams &check_config) {
+    std::vector<float> rList = check_config.radiusList;
+    std::vector<int> nList = check_config.numberList;
+
+    // Check that the size of radiusList and numberList are equal and positive
+    if (rList.size() == 0) {
+        throw std::invalid_argument("No parameters in radiusList!");
+    } else if (nList.size() == 0) {
+        throw std::invalid_argument("No parameters in numberList!");
+    } else if (rList.size() != nList.size()) {
+        throw std::invalid_argument(
+          "radiusList and numberList are of unequal size!");
+    }
+
+    // Ensure all values of radiusList are positive
+    std::vector<float>::iterator rListIterator;
+
+    for (rListIterator = rList.begin(); rListIterator != rList.end();
+         rListIterator++) {
+        if (*rListIterator < 0) {
+            throw std::invalid_argument("radiusList has a negative parameter!");
+        }
+    }
+
+    // Ensure all values of numberList are positive
+    std::vector<int>::iterator nListIterator;
+
+    for (nListIterator = nList.begin(); nListIterator != nList.end();
+         nListIterator++) {
+        if (*nListIterator < 0) {
+            throw std::invalid_argument("numberList has a negative parameter!");
+        }
+    }
+
+    // Ensure dMax and dMin are both positive, and ensure dMax is less than dMin
+    if (check_config.dMax < 0) {
+        throw std::invalid_argument("dMax is a negative value!");
+    } else if (check_config.dMin < 0) {
+        throw std::invalid_argument("dMin is a negative value!");
+    } else if (check_config.dMax > check_config.dMin) {
+        throw std::invalid_argument("dMax is greater than dMin!");
+    }
+}
+
 }  // End of namespace wave
