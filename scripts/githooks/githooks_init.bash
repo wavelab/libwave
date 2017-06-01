294e847b0f5e1f1a054b0d69a5d19c075b204555
Chris Choi <chutsu@gmail.com>
2017-05-31 22:52:22 -0400

    Fix typo

--- scripts/githooks/githooks_init.bash
+++ scripts/githooks/githooks_init.bash
@@ -4,4 +4,4 @@ rm .git/hooks/pre-commit
 ln -s $PWD/scripts/githooks/pre-commit .git/hooks/pre-commit
 
 rm .git/hooks/pre-push
-ln -s $PWD/scripts/githooks/pre-commit .git/hooks/pre-push
+ln -s $PWD/scripts/githooks/pre-push .git/hooks/pre-push
