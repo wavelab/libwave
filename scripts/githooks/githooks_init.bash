5b247169e721fbdce250a92076f81a202a5f2371
Chris Choi <chutsu@gmail.com>
2017-05-31 22:51:39 -0400

    Update githooks init

--- scripts/githooks/githooks_init.bash
+++ scripts/githooks/githooks_init.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
+
+rm .git/hooks/pre-commit
 ln -s $PWD/scripts/githooks/pre-commit .git/hooks/pre-commit
+
+rm .git/hooks/pre-push
 ln -s $PWD/scripts/githooks/pre-commit .git/hooks/pre-push
