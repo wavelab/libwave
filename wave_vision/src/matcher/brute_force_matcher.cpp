2977eb1c9117493ff7d0656de5402fec6bf248c2
Nav Ganti <navganti.12@gmail.com>
2017-06-08 10:49:07 -0400

    clang...

--- wave_vision/src/matcher/brute_force_matcher.cpp
+++ wave_vision/src/matcher/brute_force_matcher.cpp
@@ -57,6 +57,7 @@ std::vector<cv::DMatch> BruteForceMatcher::matchDescriptors(
   cv::Mat &descriptors_1, cv::Mat &descriptors_2) {
     std::vector<cv::DMatch> matches;
 
+    return matches;
 }
 
 
