69b357aff8ab77d7314f66b141f3305f6d738ce0
Nav Ganti <navganti.12@gmail.com>
2017-06-11 15:29:27 -0400

    Removed test fixture, using local vars now

--- wave_vision/tests/matcher_tests/brute_force_tests.cpp
+++ wave_vision/tests/matcher_tests/brute_force_tests.cpp
@@ -10,45 +10,6 @@ const auto TEST_CONFIG = "tests/config/matcher/brute_force_matcher.yaml";
 const auto TEST_IMAGE_1 = "tests/data/image_center.png";
 const auto TEST_IMAGE_2 = "tests/data/image_right.png";
 
-// Test fixture to load image data
-class BFTest : public testing::Test {
- protected:
-    BFTest() {
-        initDetector();
-        initDescriptor();
-        initMatcher();
-        this->image_1 = cv::imread(TEST_IMAGE_1, cv::IMREAD_COLOR);
-        this->image_2 = cv::imread(TEST_IMAGE_2, cv::IMREAD_COLOR);
-        this->keypoints_1 = this->fast.detectFeatures(this->image_1);
-        this->keypoints_2 = this->fast.detectFeatures(this->image_2);
-        this->descriptors_1 =
-          this->brisk.extractDescriptors(this->image_1, this->keypoints_1);
-        this->descriptors_2 =
-          this->brisk.extractDescriptors(this->image_2, this->keypoints_2);
-    }
-
-    virtual ~BFTest() {}
-
-    void initDetector() {
-        this->fast = FASTDetector();
-    }
-
-    void initDescriptor() {
-        this->brisk = BRISKDescriptor();
-    }
-
-    void initMatcher() {
-        this->bfmatcher = BruteForceMatcher(TEST_CONFIG);
-    }
-
-    cv::Mat image_1, image_2;
-    std::vector<cv::KeyPoint> keypoints_1, keypoints_2;
-    cv::Mat descriptors_1, descriptors_2;
-    FASTDetector fast;
-    BRISKDescriptor brisk;
-    BruteForceMatcher bfmatcher;
-};
-
 // Checks that correct configuration can be loaded
 TEST(BFTests, GoodInitialization) {
     ASSERT_NO_THROW(BruteForceMatcher bfmatcher(TEST_CONFIG));
@@ -117,21 +78,30 @@ TEST(BFTests, BadNormType) {
     ASSERT_THROW(BruteForceMatcher bfmatcher(config_nd), std::invalid_argument);
 }
 
-TEST_F(BFTest, DISABLED_MatchDescriptors) {
+TEST(BFTests, DISABLED_MatchDescriptors) {
     std::vector<cv::DMatch> matches;
     cv::Mat img_with_matches;
 
+    cv::Mat image_1, image_2;
+    std::vector<cv::KeyPoint> keypoints_1, keypoints_2;
+    cv::Mat descriptors_1, descriptors_2;
+    FASTDetector fast;
+    BRISKDescriptor brisk;
+    BruteForceMatcher bfmatcher;
+
+    image_1 = cv::imread(TEST_IMAGE_1, cv::IMREAD_COLOR);
+    image_2 = cv::imread(TEST_IMAGE_2, cv::IMREAD_COLOR);
+    keypoints_1 = fast.detectFeatures(image_1);
+    keypoints_2 = fast.detectFeatures(image_2);
+    descriptors_1 = brisk.extractDescriptors(image_1, keypoints_1);
+    descriptors_2 = brisk.extractDescriptors(image_2, keypoints_2);
+
     // Match descriptors from image 1 and image 2
-    matches = this->bfmatcher.matchDescriptors(this->descriptors_1,
-                                               this->descriptors_2);
+    matches = bfmatcher.matchDescriptors(descriptors_1, descriptors_2);
 
     // Test has been confirmed visually
-    cv::drawMatches(this->image_1,
-                    this->keypoints_1,
-                    this->image_2,
-                    this->keypoints_2,
-                    matches,
-                    img_with_matches);
+    cv::drawMatches(
+      image_1, keypoints_1, image_2, keypoints_2, matches, img_with_matches);
 
     cv::imshow("matches", img_with_matches);
 
