7b1343236d3ea2c4f7f939320a3eeea51c29cb78
Leo Koppel <lkoppel@uwaterloo.ca>
2017-06-23 16:42:57 -0400

    Add template helpers file
    This file will contain TMP helpers.
    
    Later existing templates like index_sequence should be moved here.

---
+++ wave_optimization/include/wave/optimization/factor_graph/template_helpers.hpp
@@ -0,0 +1,19 @@
+/**
+ * @file
+ * @ingroup optimization
+ * Internal: utility functions for template metaprogramming
+ */
+
+#ifndef WAVE_OPTIMIZATION_FACTOR_GRAPH_TEMPLATE_HELPERS_HPP
+#define WAVE_OPTIMIZATION_FACTOR_GRAPH_TEMPLATE_HELPERS_HPP
+
+
+namespace wave {
+/** @addtogroup optimization
+ *  @{ */
+
+namespace internal {}  // namespace internal
+/** @} group optimization */
+}  // namespace wave
+
+#endif  // WAVE_OPTIMIZATION_FACTOR_GRAPH_TEMPLATE_HELPERS_HPP
