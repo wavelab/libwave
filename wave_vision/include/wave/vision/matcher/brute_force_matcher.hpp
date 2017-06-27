b3be8fcdc7ee1fa91993d113440df943a17ead25
Nav Ganti <navganti.12@gmail.com>
2017-06-27 14:24:43 -0400

    Move BFMatcherParams from .hpp to .cpp

--- wave_vision/include/wave/vision/matcher/brute_force_matcher.hpp
+++ wave_vision/include/wave/vision/matcher/brute_force_matcher.hpp
@@ -37,36 +37,7 @@ struct BFMatcherParams {
      *
      *  @param config_path the path to the location of the configuration file
      */
-    BFMatcherParams(const std::string &config_path) {
-        // Extract parameters from .yaml file.
-        ConfigParser parser;
-
-        int norm_type;
-        bool use_knn;
-        double ratio_threshold;
-        int distance_threshold;
-        int fm_method;
-
-        // Add parameters to parser, to be loaded. If path cannot be found,
-        // throw an
-        // exception.
-        parser.addParam("norm_type", &norm_type);
-        parser.addParam("use_knn", &use_knn);
-        parser.addParam("ratio_threshold", &ratio_threshold);
-        parser.addParam("distance_threshold", &distance_threshold);
-        parser.addParam("fm_method", &fm_method);
-
-        if (parser.load(config_path) != 0) {
-            throw std::invalid_argument(
-              "Failed to Load BRISKDescriptor Configuration");
-        }
-
-        this->norm_type = norm_type;
-        this->use_knn = use_knn;
-        this->ratio_threshold = ratio_threshold;
-        this->distance_threshold = distance_threshold;
-        this->fm_method = fm_method;
-    }
+    BFMatcherParams(const std::string &config_path);
 
     /** Norm type to use for distance calculation between feature descriptors.
      *
