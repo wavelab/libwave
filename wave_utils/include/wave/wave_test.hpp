001dd1519fd5b0ed20bcca231e009772f3473ad5
Leo Koppel <lkoppel@uwaterloo.ca>
2017-07-04 12:08:11 -0400

    Add VectorsNearWithPrec

--- wave_utils/include/wave/wave_test.hpp
+++ wave_utils/include/wave/wave_test.hpp
@@ -23,6 +23,24 @@ inline bool MatricesNear(const MatX &m1, const MatX &m2) {
     return m1.isApprox(m2);
 }
 
+/** Predicate to check if vectors are equal within the given precision.
+ * Use with EXPECT_PRED3
+ * @note The comparison is multiplicative, and prec is not a linear tolerance.
+ * See the documentation for Eigen's `isApprox()`.
+ */
+inline bool VectorsNearWithPrec(const VecX &v1, const VecX &v2, double prec) {
+    return v1.isApprox(v2, prec);
+}
+
+/** Predicate to check if matrices are equal within the given precision.
+ * Use with EXPECT_PRED3
+ * @note The comparison is multiplicative, and prec is not a linear tolerance.
+ * See the documentation for Eigen's `isApprox()`.
+ */
+inline bool MatricesNearWithPrec(const MatX &m1, const MatX &m2, double prec) {
+    return m1.isApprox(m2, prec);
+}
+
 }  // namespace wave
 
 #endif  // WAVE_TEST_HPP
