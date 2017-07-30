980b2b830e2dc9e0df5136ca1427e045b4dede5d
Leo Koppel <lkoppel@uwaterloo.ca>
2017-07-30 19:21:39 -0400

    Update gtsam_offline_kitti_example

--- wave_optimization/tests/gtsam/gtsam_offline_kitti_example.cpp
+++ wave_optimization/tests/gtsam/gtsam_offline_kitti_example.cpp
@@ -1,31 +1,28 @@
-#include <gtsam/geometry/Pose2.h>
-#include <gtsam/geometry/Pose3.h>
 #include <gtsam/slam/PriorFactor.h>
 #include <gtsam/slam/ProjectionFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+
 #include <gtsam/nonlinear/NonlinearFactorGraph.h>
 #include <gtsam/nonlinear/LevenbergMarquardtOptimizer.h>
 #include <gtsam/inference/Symbol.h>
 
 #include "wave/wave_test.hpp"
 #include "wave/vision/dataset.hpp"
+#include "gtsam_helpers.hpp"
 
 const auto DATASET_DIR = "tests/data/vo_data_drive_0036";
 
 namespace wave {
 
-/** Convert pose to gtsam type */
-inline gtsam::Pose3 gtsamPoseFromEigen(const Quaternion &q, const Vec3 &p) {
-    return gtsam::Pose3{gtsam::Rot3{q}, gtsam::Point3{p}};
-}
-
 class GtsamExample : public ::testing::Test {
  protected:
     VOTestDataset dataset = VOTestDataset::loadFromDirectory(DATASET_DIR);
-    gtsam::Cal3_S2 kParams{dataset.camera_K(0, 0),
-                           dataset.camera_K(1, 1),
-                           0.,
-                           dataset.camera_K(0, 2),
-                           dataset.camera_K(1, 2)};
+    gtsam::Cal3_S2::shared_ptr kParams =
+      boost::make_shared<gtsam::Cal3_S2>(dataset.camera_K(0, 0),
+                                         dataset.camera_K(1, 1),
+                                         0.,
+                                         dataset.camera_K(0, 2),
+                                         dataset.camera_K(1, 2));
 };
 
 TEST_F(GtsamExample, run) {
@@ -35,7 +32,7 @@ TEST_F(GtsamExample, run) {
     std::vector<gtsam::Pose3> true_poses;
 
     // Noise: one pixel in u and v
-    auto measurement_noise = gtsam::noiseModel::Isotropic::Sigma(2, 10.0);
+    const auto measurement_noise = gtsam::noiseModel::Isotropic::Sigma(2, 10.0);
 
     // Loop over the different poses, adding the observations to iSAM
     // incrementally
@@ -46,22 +43,29 @@ TEST_F(GtsamExample, run) {
         // We have to convert things to gtsam types
         // @todo better integration
 
-        // Transform from robot to camera frame
-        // This involves the rotation sequence: -90 deg about initial x axis,
-        // 0, then -90 deg about initial z axis.
-        auto q_BC = Quaternion{Eigen::AngleAxisd(-M_PI_2, Vec3::UnitZ()) *
-                               Eigen::AngleAxisd(-M_PI_2, Vec3::UnitX())};
-
-        const Quaternion q_GC = state.robot_q_GB * q_BC;
-        const auto &G_p_GC = state.robot_G_p_GB;
-        auto pose = gtsamPoseFromEigen(q_GC, G_p_GC);
+        const auto pose = gtsamPoseFromState(state);
         true_poses.push_back(pose);
 
         // Add a purposely offset initial estimate
-        auto offset = gtsam::Pose3{gtsam::Rot3::Rodrigues(-0.1, 0.2, 0.25),
-                                   gtsam::Point3{0.05, -0.10, 0.20}};
+        const auto offset = gtsam::Pose3{gtsam::Rot3::Rodrigues(-0.1, 0.1, 0.1),
+                                         gtsam::Point3{0.05, -0.10, 0.20}};
 
-        initial_estimate.insert(gtsam::Symbol{'x', i}, pose.compose(offset));
+        auto pose_estimate = pose;
+        pose_estimate = pose_estimate.compose(offset);
+        initial_estimate.insert(gtsam::Symbol{'x', i}, pose_estimate);
+
+        if (i > 0) {
+            // Add odometry factor
+            auto between = poseBetweenStates(dataset.states[i - 1], state);
+
+            Vec6 noise_vec;
+            noise_vec << Vec3::Constant(0.001), Vec3::Constant(0.0001);
+
+            auto noise = gtsam::noiseModel::Diagonal::Sigmas(noise_vec);
+            auto odometry_factor = gtsam::BetweenFactor<gtsam::Pose3>{
+              gtsam::Symbol{'x', i - 1}, gtsam::Symbol{'x', i}, between, noise};
+            graph.push_back(odometry_factor);
+        }
 
         // Add factors for each landmark observation
         for (size_t j = 0; j < observations.size(); ++j) {
@@ -69,19 +73,21 @@ TEST_F(GtsamExample, run) {
             const auto measurement = gtsam::Point2{observations[j].second};
             const auto landmark_id = observations[j].first;
 
-            graph.push_back(gtsam::GenericProjectionFactor<gtsam::Pose3,
-                                                           gtsam::Point3,
-                                                           gtsam::Cal3_S2>{
-              measurement,
-              measurement_noise,
-              gtsam::Symbol{'x', i},
-              gtsam::Symbol{'l', landmark_id},
-              boost::make_shared<gtsam::Cal3_S2>(this->kParams)});
+            auto projection_factor =
+              gtsam::GenericProjectionFactor<gtsam::Pose3,
+                                             gtsam::Point3,
+                                             gtsam::Cal3_S2>{
+                measurement,
+                measurement_noise,
+                gtsam::Symbol{'x', i},
+                gtsam::Symbol{'l', landmark_id},
+                this->kParams};
+            graph.push_back(projection_factor);
 
             // Initialize the landmark just in front of the camera
             const auto key = gtsam::Symbol{'l', landmark_id};
             if (!initial_estimate.exists(key)) {
-                auto camera = gtsam::SimpleCamera{pose, this->kParams};
+                auto camera = gtsam::SimpleCamera{pose, *this->kParams};
                 auto est = camera.backproject_from_camera(measurement, 3.0);
                 initial_estimate.insert<gtsam::Point3>(key, est);
             }
@@ -111,20 +117,22 @@ TEST_F(GtsamExample, run) {
         const auto estimated_pose = result.at(key).cast<gtsam::Pose3>();
         const auto &true_pose = true_poses[i];
 
-        EXPECT_PRED3(VectorsNearPrec,
-                     true_pose.translation(),
-                     estimated_pose.translation(),
-                     2.0)
-          << "x" << i;
-        // @todo: improve accuracy
+        double linear_dist =
+          (true_pose.translation() - estimated_pose.translation()).norm();
+        EXPECT_LT(linear_dist, 1e-2) << "x" << i;
 
         const auto true_q = true_poses[i].rotation().toQuaternion();
         const auto estimated_q = estimated_pose.rotation().toQuaternion();
         const auto rotation_error = true_q.angularDistance(estimated_q);
-        EXPECT_LT(rotation_error, 1.0) << "x" << i;
+        EXPECT_LT(rotation_error, 1e-3) << "x" << i;
     }
 
     // No landmark ground truth to check in this case
+
+    std::cout << "gtsam_offline_kitti_example results:" << std::endl;
+    std::cout << "initial error = " << graph.error(initial_estimate)
+              << std::endl;
+    std::cout << "final error = " << graph.error(result) << std::endl;
 }
 
 }  // namespace wave
