83e722797e174ac94bc99e7a543a8ef86874c487
Nav Ganti <navganti.12@gmail.com>
2017-08-07 21:09:00 -0400

    Fix cv::makePtr usage

--- wave_vision/src/matcher/flann_matcher.cpp
+++ wave_vision/src/matcher/flann_matcher.cpp
@@ -16,26 +16,28 @@ FLANNMatcher::FLANNMatcher(const FLANNMatcherParams &config) {
         cv::flann::KDTreeIndexParams kdtree_params;
 
         // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(cv::makePtr(kdtree_params),
-                                      cv::makePtr(search_params));
-        this->flann_matcher = cv::makePtr(matcher);
+        cv::FlannBasedMatcher matcher(
+          cv::makePtr<cv::flann::KDTreeIndexParams>(kdtree_params),
+          cv::makePtr<cv::flann::SearchParams>(search_params));
+        this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::KMeans) {
         // Create KMeans index params with default values.
         cv::flann::KMeansIndexParams kmeans_params;
 
         // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(cv::makePtr(kmeans_params),
-                                      cv::makePtr(search_params));
-        this->flann_matcher = cv::makePtr(matcher);
-
+        cv::FlannBasedMatcher matcher(
+          cv::makePtr<cv::flann::KMeansIndexParams>(kmeans_params),
+          cv::makePtr<cv::flann::SearchParams>(search_params));
+        this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::Composite) {
         // Create composite index params with default values.
         cv::flann::CompositeIndexParams composite_params;
 
         // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(cv::makePtr(composite_params),
-                                      cv::makePtr(search_params));
-        this->flann_matcher = cv::makePtr(matcher);
+        cv::FlannBasedMatcher matcher(
+          cv::makePtr<cv::flann::CompositeIndexParams>(composite_params),
+          cv::makePtr<cv::flann::SearchParams>(search_params));
+        this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::LSH) {
         // Create LSH params with default values. These are values recommended
         // by Kaehler and Bradski - the LSH struct in OpenCV does not have
@@ -47,24 +49,27 @@ FLANNMatcher::FLANNMatcher(const FLANNMatcherParams &config) {
           num_tables, key_size, multi_probe_level);
 
         // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(cv::makePtr(lsh_params),
-                                      cv::makePtr(search_params));
-        this->flann_matcher = cv::makePtr(matcher);
+        cv::FlannBasedMatcher matcher(
+          cv::makePtr<cv::flann::LshIndexParams>(lsh_params),
+          cv::makePtr<cv::flann::SearchParams>(search_params));
+        this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::Autotuned) {
         // Create autotuned index params with default values
         cv::flann::AutotunedIndexParams autotuned_params;
 
         // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(cv::makePtr(autotuned_params),
-                                      cv::makePtr(search_params));
-        this->flann_matcher = cv::makePtr(matcher);
+        cv::FlannBasedMatcher matcher(
+          cv::makePtr<cv::flann::AutotunedIndexParams>(autotuned_params),
+          cv::makePtr<cv::flann::SearchParams>(search_params));
+        this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     }
 
     this->current_config = config;
 }
 
-void FLANNMatcher::checkConfiguration(const int &flann_method) {
-    if (flann_method < FLANN::KDTree || flann_method > FLANN::Autotuned) {
+void FLANNMatcher::checkConfiguration(const FLANNMatcherParams &config) {
+    if (config.flann_method < FLANN::KDTree ||
+        config.flann_method > FLANN::Autotuned) {
         throw std::invalid_argument("Flann method selected does not exist!");
     }
 }
