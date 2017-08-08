4aa4beac5005d534ac646b1dadf606441df20ed4
Nav Ganti <navganti.12@gmail.com>
2017-08-08 12:01:11 -0400

    FLANN tests'

--- wave_vision/src/matcher/flann_matcher.cpp
+++ wave_vision/src/matcher/flann_matcher.cpp
@@ -40,37 +40,21 @@ FLANNMatcher::FLANNMatcher(const FLANNMatcherParams &config) {
     // Check flann_method and create the appropriate parameters struct.
     this->checkConfiguration(config);
 
-    // Create search params with default values. This is used for all
-    // matcher implementations.
-    cv::flann::SearchParams search_params;
-
     // Depending on the FLANN method, different parameters are required.
     if (config.flann_method == FLANN::KDTree) {
-        // Create KDTree index params and search params with default values.
-        cv::flann::KDTreeIndexParams kdtree_params;
-
-        // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(
-          cv::makePtr<cv::flann::KDTreeIndexParams>(kdtree_params),
-          cv::makePtr<cv::flann::SearchParams>(search_params));
+        // Create FLANN matcher with default KDTree and Search params.
+        cv::FlannBasedMatcher matcher(new cv::flann::KDTreeIndexParams(),
+                                      new cv::flann::SearchParams());
         this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::KMeans) {
-        // Create KMeans index params with default values.
-        cv::flann::KMeansIndexParams kmeans_params;
-
-        // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(
-          cv::makePtr<cv::flann::KMeansIndexParams>(kmeans_params),
-          cv::makePtr<cv::flann::SearchParams>(search_params));
+        // Create FLANN matcher with default KMeans and Search params
+        cv::FlannBasedMatcher matcher(new cv::flann::KMeansIndexParams(),
+                                      new cv::flann::SearchParams());
         this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::Composite) {
-        // Create composite index params with default values.
-        cv::flann::CompositeIndexParams composite_params;
-
-        // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(
-          cv::makePtr<cv::flann::CompositeIndexParams>(composite_params),
-          cv::makePtr<cv::flann::SearchParams>(search_params));
+        // Create FLANN matcher with default Composite and Search params
+        cv::FlannBasedMatcher matcher(new cv::flann::CompositeIndexParams(),
+                                      new cv::flann::SearchParams());
         this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::LSH) {
         // Create LSH params with default values. These are values recommended
@@ -79,22 +63,17 @@ FLANNMatcher::FLANNMatcher(const FLANNMatcherParams &config) {
         unsigned int num_tables = 20;  // Typically between 10-30
         unsigned int key_size = 15;    // Typically between 10-20
         unsigned int multi_probe_level = 2;
-        cv::flann::LshIndexParams lsh_params(
-          num_tables, key_size, multi_probe_level);
 
-        // Create FLANN matcher
+        // Create FLANN matcher with default LSH and Search params
         cv::FlannBasedMatcher matcher(
-          cv::makePtr<cv::flann::LshIndexParams>(lsh_params),
-          cv::makePtr<cv::flann::SearchParams>(search_params));
+          new cv::flann::LshIndexParams(
+            num_tables, key_size, multi_probe_level),
+          new cv::flann::SearchParams());
         this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     } else if (config.flann_method == FLANN::Autotuned) {
-        // Create autotuned index params with default values
-        cv::flann::AutotunedIndexParams autotuned_params;
-
-        // Create FLANN matcher
-        cv::FlannBasedMatcher matcher(
-          cv::makePtr<cv::flann::AutotunedIndexParams>(autotuned_params),
-          cv::makePtr<cv::flann::SearchParams>(search_params));
+        // Create FLANN matcher with default Autotuned Params
+        cv::FlannBasedMatcher matcher(new cv::flann::AutotunedIndexParams(),
+                                      new cv::flann::SearchParams());
         this->flann_matcher = cv::makePtr<cv::FlannBasedMatcher>(matcher);
     }
 
@@ -108,21 +87,16 @@ void FLANNMatcher::checkConfiguration(const FLANNMatcherParams &check_config) {
         throw std::invalid_argument("Flann method selected does not exist!");
     }
 
-    // If use_knn is true, check ratio_threshold value. Otherwise check
-    // distance_threshold
-    if (check_config.use_knn) {
-        // Check the value of the ratio_test heuristic
-        if (check_config.ratio_threshold < 0.0 ||
-            check_config.ratio_threshold > 1.0) {
-            throw std::invalid_argument(
-              "ratio_threshold is not an appropriate value!");
-        }
-    } else {
-        // Check the value of the threshold distance heuristic
-        if (check_config.distance_threshold < 0) {
-            throw std::invalid_argument(
-              "distance_threshold is a negative value!");
-        }
+    // Check the value of the ratio_test heuristic
+    if (check_config.ratio_threshold < 0.0 ||
+        check_config.ratio_threshold > 1.0) {
+        throw std::invalid_argument(
+          "ratio_threshold is not an appropriate value!");
+    }
+
+    // Check the value of the threshold distance heuristic
+    if (check_config.distance_threshold < 0) {
+        throw std::invalid_argument("distance_threshold is a negative value!");
     }
 
     // Only acceptable values are 1, 2, 4, and 8
---
+++ wave_vision/tests/matcher_tests/flann_tests.cpp
@@ -0,0 +1,140 @@
+#include "wave/wave_test.hpp"
+#include "wave/vision/utils.hpp"
+#include "wave/vision/detector/orb_detector.hpp"
+#include "wave/vision/descriptor/orb_descriptor.hpp"
+#include "wave/vision/matcher/flann_matcher.hpp"
+
+namespace wave {
+
+const auto TEST_CONFIG = "tests/config/matcher/flann.yaml";
+
+// Checks that default configuration has no issues
+TEST(FLANNTests, GoodConfig) {
+    // Default
+    EXPECT_NO_THROW(FLANNMatcherParams config1);
+
+    // Custom params struct (with good values)
+    int flann_method = FLANN::KDTree;
+    double ratio_threshold = 0.8;
+    int distance_threshold = 5;
+    bool auto_remove_outliers = true;
+    int fm_method = cv::FM_RANSAC;
+
+    EXPECT_NO_THROW(FLANNMatcherParams config2(
+      flann_method, ratio_threshold, auto_remove_outliers, fm_method));
+
+    EXPECT_NO_THROW(FLANNMatcherParams config3(
+      flann_method, distance_threshold, auto_remove_outliers, fm_method));
+
+    // From flann.yaml, with good values.
+    EXPECT_NO_THROW(FLANNMatcherParams config4(TEST_CONFIG));
+}
+
+// Checks that incorrect configuration path throws an exception
+TEST(FLANNTests, BadConfigPath) {
+    const std::string bad_path = "bad_path";
+
+    ASSERT_THROW(FLANNMatcherParams config(bad_path), std::invalid_argument);
+}
+
+TEST(FLANNTests, ConstructorTest) {
+    FLANNMatcherParams config;
+
+    EXPECT_NO_THROW(FLANNMatcher matcher);
+    EXPECT_NO_THROW(FLANNMatcher matcher1(config));
+
+    // Check all methods
+    config.flann_method = FLANN::KMeans;
+    EXPECT_NO_THROW(FLANNMatcher matcher2(config));
+
+    config.flann_method = FLANN::Composite;
+    EXPECT_NO_THROW(FLANNMatcher matcher3(config));
+
+    config.flann_method = FLANN::LSH;
+    EXPECT_NO_THROW(FLANNMatcher matcher4(config));
+
+    config.flann_method = FLANN::Autotuned;
+    EXPECT_NO_THROW(FLANNMatcher matcher5(config));
+}
+
+// Check that incorrect parameter values throw exceptions.
+TEST(FLANNTests, BadFLANNMethod) {
+    FLANNMatcherParams config;
+    config.flann_method = 0;
+    ASSERT_THROW(FLANNMatcher bad_norm1(config), std::invalid_argument);
+
+    config.flann_method = 6;
+    ASSERT_THROW(FLANNMatcher bad_norm2(config), std::invalid_argument);
+}
+
+TEST(FLANNTests, BadRatioTestHeuristic) {
+    FLANNMatcherParams config;
+    config.ratio_threshold = -0.5;
+    ASSERT_THROW(FLANNMatcher bad_rth1(config), std::invalid_argument);
+
+    config.ratio_threshold = 1.5;
+    ASSERT_THROW(FLANNMatcher bad_rth2(config), std::invalid_argument);
+}
+
+TEST(FLANNTests, BadRejectionHeuristic) {
+    FLANNMatcherParams config;
+    config.distance_threshold = -1;
+    ASSERT_THROW(FLANNMatcher bad_rh(config), std::invalid_argument);
+}
+
+TEST(FLANNTests, BadFmMethod) {
+    FLANNMatcherParams config;
+    config.fm_method = -1;
+
+    ASSERT_THROW(FLANNMatcher bad_fm1(config), std::invalid_argument);
+
+    config.fm_method = 9;
+    ASSERT_THROW(FLANNMatcher bad_fm2(config), std::invalid_argument);
+
+    config.fm_method = 3;
+    ASSERT_THROW(FLANNMatcher bad_fm3(config), std::invalid_argument);
+
+    config.fm_method = 5;
+    ASSERT_THROW(FLANNMatcher bad_fm4(config), std::invalid_argument);
+}
+
+TEST(FLANNTests, ConfigurationTests) {
+    FLANNMatcherParams ref_config;
+    FLANNMatcherParams yaml_config(TEST_CONFIG);
+
+    FLANNMatcher matcher1(ref_config);
+    FLANNMatcher matcher2;
+    FLANNMatcher matcher3(yaml_config);
+
+    FLANNMatcherParams curr_config_1 = matcher1.getConfiguration();
+    FLANNMatcherParams curr_config_2 = matcher2.getConfiguration();
+    FLANNMatcherParams curr_config_3 = matcher3.getConfiguration();
+
+    // Confirm values for construction with custom struct
+    ASSERT_EQ(curr_config_1.flann_method, ref_config.flann_method);
+    ASSERT_EQ(curr_config_1.use_knn, ref_config.use_knn);
+    ASSERT_EQ(curr_config_1.ratio_threshold, ref_config.ratio_threshold);
+    ASSERT_EQ(curr_config_1.distance_threshold, ref_config.distance_threshold);
+    ASSERT_EQ(curr_config_1.auto_remove_outliers,
+              ref_config.auto_remove_outliers);
+    ASSERT_EQ(curr_config_1.fm_method, ref_config.fm_method);
+
+    // Confirm default construction
+    ASSERT_EQ(curr_config_2.flann_method, ref_config.flann_method);
+    ASSERT_EQ(curr_config_2.use_knn, ref_config.use_knn);
+    ASSERT_EQ(curr_config_2.ratio_threshold, ref_config.ratio_threshold);
+    ASSERT_EQ(curr_config_2.distance_threshold, ref_config.distance_threshold);
+    ASSERT_EQ(curr_config_2.auto_remove_outliers,
+              ref_config.auto_remove_outliers);
+    ASSERT_EQ(curr_config_2.fm_method, ref_config.fm_method);
+
+    // Confirm construction with .yaml file
+    ASSERT_EQ(curr_config_3.flann_method, ref_config.flann_method);
+    ASSERT_EQ(curr_config_3.use_knn, ref_config.use_knn);
+    ASSERT_EQ(curr_config_3.ratio_threshold, ref_config.ratio_threshold);
+    ASSERT_EQ(curr_config_3.distance_threshold, ref_config.distance_threshold);
+    ASSERT_EQ(curr_config_3.auto_remove_outliers,
+              ref_config.auto_remove_outliers);
+    ASSERT_EQ(curr_config_3.fm_method, ref_config.fm_method);
+}
+}  // namespace wave
