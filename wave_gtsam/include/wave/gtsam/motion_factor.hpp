67527c2fb136bc4b82df80f7aee8a38bcb8ff299
Nav Ganti <navganti.12@gmail.com>
2018-03-20 11:59:36 -0400

    first attempt at templated motion factor

--- wave_gtsam/include/wave/gtsam/motion_factor.hpp
+++ wave_gtsam/include/wave/gtsam/motion_factor.hpp
@@ -3,26 +3,30 @@
 
 #include <gtsam/nonlinear/NonlinearFactor.h>
 #include <gtsam/base/Matrix.h>
+#include "wave/gtsam/pose_vel.hpp"
 #include "wave/gtsam/pose_vel_bias.hpp"
 
 /**
- * This factor is designed for used with the PoseVelBias states. It implements
- * a simple constant velocity model with forward integration:
+ * This factor implements  a simple constant velocity model with forward
+ * integration, and is designed to be used with PoseVel or PoseVelBias:
  *
  * S2 = S1 + delta_t * S1_velocity
  * S2_velocity = S2_velocity
+ *
+ * If being used with the PoseVelBias states:
  * S2_bias = S1_bias
  *
  * The error therefore has dimension 15 and is put together in the same order as
  * above
  *
  * The noise model should be used to weight each component of the error as
- * desired
+ * desired.
  */
 
 namespace wave {
 
-class MotionFactor : public gtsam::NoiseModelFactor2<PoseVelBias, PoseVelBias> {
+template <typename T>
+class MotionFactor : public gtsam::NoiseModelFactor2<T, T> {
  private:
     const double delt;
 
@@ -32,12 +36,22 @@ class MotionFactor : public gtsam::NoiseModelFactor2<PoseVelBias, PoseVelBias> {
                  double delta_t,
                  const gtsam::SharedNoiseModel &model);
 
-    gtsam::Vector evaluateError(
+    template <>
+    gtsam::Vector evaluateError<wave::PoseVelBias>(
       const wave::PoseVelBias &m1,
       const wave::PoseVelBias &m2,
       boost::optional<gtsam::Matrix &> H1 = boost::none,
       boost::optional<gtsam::Matrix &> H2 = boost::none) const;
+
+    template <>
+    gtsam::Vector evaluateError<wave::PoseVel>(
+      const wave::PoseVel &m1,
+      const wave::PoseVel &m2,
+      boost::optional<gtsam::Matrix &> H1 = boost::none,
+      boost::optional<gtsam::Matrix &> H2 = boost::none) const;
 };
 }
+
+#include "wave/gtsam/impl/motion_factor_impl.hpp"
 
 #endif  // WAVE_MOTION_FACTOR_HPP
