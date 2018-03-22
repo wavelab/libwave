f74d8dc2b9ac602723d611cbc3a3d2ed7b946406
Nav Ganti <navganti@users.noreply.github.com>
2018-03-22 18:31:01 -0400

    Enhance 275 GTSAM localizer relative estimate (#277)
    * update localizer state, fix potential issue with pose_vel_bias
    
    * add localizer_state.cpp
    
    * remove trailing .
    
    * add pose and vel offset to pose_vel
    
    * move pose_vel operator to separate file
    
    * remove unfinished function
    
    * remove pass-by reference
    
    * first attempt at templated motion factor
    
    * remove motionfactor.cpp from cmake
    
    * test templated PoseVelBias, works
    
    * change class to typename
    
    * add template specialization
    
    * delete previous localizer_state formulation
    
    * change template speciialization to overload
    
    * revert to template specialization
    
    * add pose_vel tests
    
    * fix up ordering
    
    * make templated functions inline, else they violate one definition
    
    * clang

---
+++ wave_gtsam/include/wave/gtsam/impl/motion_factor_impl.hpp
@@ -0,0 +1,68 @@
+#ifndef WAVE_MOTION_FACTOR_IMPL_HPP
+#define WAVE_MOTION_FACTOR_IMPL_HPP
+
+#include "wave/gtsam/motion_factor.hpp"
+
+namespace wave {
+template <>
+inline gtsam::Vector
+MotionFactor<wave::PoseVelBias, wave::PoseVelBias>::evaluateError(
+  const wave::PoseVelBias &m1,
+  const wave::PoseVelBias &m2,
+  boost::optional<gtsam::Matrix &> H1,
+  boost::optional<gtsam::Matrix &> H2) const {
+    gtsam::Vector retval;
+
+    if (H1) {
+        H1->resize(15, 15);
+        H1->setIdentity();
+
+        H1->block<6, 6>(0, 6).noalias() =
+          this->delt * gtsam::Matrix6::Identity();
+    }
+
+    if (H2) {
+        H2->resize(15, 15);
+        *H2 = -1 * Eigen::Matrix<double, 15, 15>::Identity();
+    }
+
+    retval.resize(15, 1);
+    retval.block<6, 1>(0, 0).noalias() =
+      m1.vel * this->delt - m1.pose.localCoordinates(m2.pose);
+    retval.block<6, 1>(6, 0).noalias() = m1.vel - m2.vel;
+    retval.block<3, 1>(12, 0).noalias() = m1.bias - m2.bias;
+
+    return retval;
+}
+
+template <>
+inline gtsam::Vector MotionFactor<wave::PoseVel, wave::PoseVel>::evaluateError(
+  const wave::PoseVel &m1,
+  const wave::PoseVel &m2,
+  boost::optional<gtsam::Matrix &> H1,
+  boost::optional<gtsam::Matrix &> H2) const {
+    gtsam::Vector retval;
+
+    if (H1) {
+        H1->resize(12, 12);
+        H1->setIdentity();
+
+        H1->block<6, 6>(0, 6).noalias() =
+          this->delt * gtsam::Matrix6::Identity();
+    }
+
+    if (H2) {
+        H2->resize(12, 12);
+        *H2 = -1 * Eigen::Matrix<double, 12, 12>::Identity();
+    }
+
+    retval.resize(12, 1);
+    retval.block<6, 1>(0, 0).noalias() =
+      m1.vel * this->delt - m1.pose.localCoordinates(m2.pose);
+    retval.block<6, 1>(6, 0).noalias() = m1.vel - m2.vel;
+
+    return retval;
+}
+}  // namespace wave
+
+#endif  // WAVE_MOTION_FACTOR_IMPL_HPP
--- wave_gtsam/tests/gtsam/pose_vel_state_test.cpp
+++ wave_gtsam/tests/gtsam/pose_vel_state_test.cpp
@@ -7,9 +7,124 @@
 #include <gtsam/slam/BetweenFactor.h>
 #include "wave/gtsam/motion_factor.hpp"
 #include "wave/gtsam/pose_vel_bias.hpp"
+#include "wave/gtsam/pose_vel.hpp"
 #include "wave/wave_test.hpp"
 
 namespace wave {
+
+TEST(pose_vel_state, example) {
+    uint64_t from, to;
+    double delta_t = 0.1;
+    from = 1;
+    to = 2;
+
+    Eigen::Matrix<double, 12, 12> info;
+    info.setIdentity();
+
+    auto noise = gtsam::noiseModel::Gaussian::Information(info);
+    MotionFactor<wave::PoseVel, wave::PoseVel> factor(from, to, delta_t, noise);
+
+    PoseVel Start, End;
+    Start.vel << 0, 0, 0.1, 5, 0, 0;
+    End.vel << 0, 0, 0.1, 5, 0, 0;
+
+    End.pose = Start.pose.Retract(delta_t * Start.vel);
+
+    gtsam::Matrix H1, H2;
+    Eigen::Matrix<double, 12, 1> err = factor.evaluateError(Start, End, H1, H2);
+
+    EXPECT_TRUE(err.isZero());
+}
+
+TEST(pose_vel_state, single_prior) {
+    gtsam::Values initial;
+    gtsam::NonlinearFactorGraph graph;
+
+    PoseVel state;
+    initial.insert(1, state);
+
+    Eigen::Affine3d T_local_s1;
+    T_local_s1.matrix() << 0.936293363584199, -0.275095847318244,
+      0.218350663146334, 32.000000000000000, 0.289629477625516,
+      0.956425085849232, -0.036957013524625, 2.000000000000000,
+      -0.198669330795061, 0.097843395007256, 0.975170327201816,
+      3.000000000000000, 0, 0, 0, 1.000000000000000;
+
+    gtsam::Pose3 prior_pose(T_local_s1.matrix());
+    state.pose = prior_pose;
+    state.vel << -0.4, 0.3, 0.1, 5, 2, 1;
+
+    Eigen::Matrix<double, 12, 12> info;
+    info.setIdentity();
+    auto model = gtsam::noiseModel::Gaussian::Information(info);
+    auto factor = gtsam::PriorFactor<PoseVel>(1, state, model);
+    graph.add(factor);
+
+    gtsam::LevenbergMarquardtOptimizer optimizer(graph, initial);
+    auto result = optimizer.optimize();
+
+    EXPECT_TRUE(gtsam::traits<PoseVel>::Equals(state, result.at<PoseVel>(1)));
+}
+
+TEST(pose_vel_state, logmap) {
+    Eigen::Affine3d T_local_s1;
+    T_local_s1.matrix() << 0.936293363584199, -0.275095847318244,
+      0.218350663146334, 32.000000000000000, 0.289629477625516,
+      0.956425085849232, -0.036957013524625, 2.000000000000000,
+      -0.198669330795061, 0.097843395007256, 0.975170327201816,
+      3.000000000000000, 0, 0, 0, 1.000000000000000;
+
+    PoseVel state;
+    gtsam::Pose3 prior_pose(T_local_s1.matrix());
+    state.pose = prior_pose;
+    state.vel << -0.4, 0.3, 0.1, 5, 2, 1;
+
+    gtsam::Matrix H_combined, H_pose, H_vel;
+
+    // Expected values from separate objects
+    auto tangent_pose = gtsam::Pose3::Logmap(state.pose, H_pose);
+    auto tangent_vel = gtsam::traits<gtsam::Vector6>::Logmap(state.vel, H_vel);
+
+    // Actual value
+    auto tangent_combined = gtsam::traits<PoseVel>::Logmap(state, H_combined);
+
+    EXPECT_PRED2(VectorsNear, tangent_combined.segment<6>(0), tangent_pose);
+    EXPECT_PRED2(VectorsNear, tangent_combined.segment<6>(6), tangent_vel);
+    EXPECT_PRED2(MatricesNear, (H_combined.block<6, 6>(0, 0)), H_pose);
+    EXPECT_PRED2(MatricesNear, (H_combined.block<6, 6>(6, 6)), H_vel);
+    EXPECT_TRUE((H_combined.block<6, 6>(0, 6)).isZero());
+}
+
+TEST(pose_vel_state, trivial_problem) {
+    gtsam::Values initial;
+    gtsam::NonlinearFactorGraph graph;
+    const double delta_t = 0.1;
+
+    std::vector<PoseVel> states(10);
+    initial.insert(0, states.at(0));
+    states.at(0).vel << 0, 0, 0.1, 5, 0, 0;
+    // Add prior factor on first state
+    auto info = Eigen::Matrix<double, 12, 12>::Identity();
+    auto model = gtsam::noiseModel::Gaussian::Information(info);
+    graph.add(gtsam::PriorFactor<wave::PoseVel>(0, states.at(0), model));
+
+    for (uint16_t i = 1; i < states.size(); i++) {
+        initial.insert(i, states.at(i));
+        states.at(i).pose =
+          states.at(i - 1).pose.retract(delta_t * states.at(i - 1).vel);
+        states.at(i).vel = states.at(i - 1).vel;
+        graph.add(
+          MotionFactor<wave::PoseVel, wave::PoseVel>(i - 1, i, delta_t, model));
+    }
+
+    gtsam::LevenbergMarquardtOptimizer optimizer(graph, initial);
+    auto result = optimizer.optimize();
+
+    for (uint64_t i = 0; i < result.size(); i++) {
+        PoseVel res = result.at<PoseVel>(i);
+        EXPECT_TRUE(gtsam::traits<PoseVel>::Equals(states.at(i), res, 1e-3));
+    }
+}
 
 TEST(pose_vel_bias_state, example) {
     uint64_t from, to;
@@ -20,7 +135,8 @@ TEST(pose_vel_bias_state, example) {
     Eigen::Matrix<double, 15, 15> info;
     info.setIdentity();
     auto noise = gtsam::noiseModel::Gaussian::Information(info);
-    MotionFactor factor(from, to, delta_t, noise);
+    MotionFactor<wave::PoseVelBias, wave::PoseVelBias> factor(
+      from, to, delta_t, noise);
 
     PoseVelBias Start, End;
     Start.vel << 0, 0, 0.1, 5, 0, 0;
@@ -119,7 +235,8 @@ TEST(pose_vel_bias_state, trivial_problem) {
         states.at(i).pose =
           states.at(i - 1).pose.retract(delta_t * states.at(i - 1).vel);
         states.at(i).vel = states.at(i - 1).vel;
-        graph.add(MotionFactor(i - 1, i, delta_t, model));
+        graph.add(MotionFactor<wave::PoseVelBias, wave::PoseVelBias>(
+          i - 1, i, delta_t, model));
     }
 
     gtsam::LevenbergMarquardtOptimizer optimizer(graph, initial);
@@ -131,5 +248,4 @@ TEST(pose_vel_bias_state, trivial_problem) {
           gtsam::traits<PoseVelBias>::Equals(states.at(i), res, 1e-3));
     }
 }
-
 }  // namespace wave
