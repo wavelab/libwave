ff1de2a3cc8318a2effd09530a4ea463f633228c
Benjamin Skikos <benjamin.skikos@gmail.com>
2018-06-15 15:10:50 -0400

    finish spherical parameterization and add test

--- wave_optimization/src/ceres/local_params/spherical_parameterization.cpp
+++ wave_optimization/src/ceres/local_params/spherical_parameterization.cpp
@@ -3,35 +3,53 @@
 namespace wave {
 
 bool SphericalParameterization::Plus(const double *x, const double *delta, double *x_plus_delta) const {
-    Eigen::Map<const Vec3> p(x);
-    Eigen::Map<const Vec2> dp(delta);
-    Eigen::Map<Vec3> ppdp(x_plus_delta);
+    Eigen::Map<const Vec3> X(x);
+    Eigen::Map<const Vec2> D(delta);
+    Eigen::Map<Vec3> XpD(x_plus_delta);
 
     // Cross product of unit z vector with p
-    Vec3 axis;
-    axis << -p(1), p(0), 0.0;
+    Vec2 axis;
+    axis << -X(1), X(0); //, 0.0;
 
-    double c = p(2);
+    double c = X(2);
 
-    Mat3 skew;
-    skew << 0, -axis(2), axis(1),
-           axis(2), 0, -axis(0),
-           -axis(1), axis(0), 0;
+    Mat3 skew = Mat3::Zero();
+    skew(0, 2) = axis(1);
+    skew(1, 2) = -axis(0);
+    skew(2, 0) = -axis(1);
+    skew(2, 1) = axis(0);
 
     Mat3 R = Mat3::Identity() + skew + (1.0 / (1.0 + c)) * skew * skew;
 
-    Vec3 v = R.block<3, 2>(0,0) * dp;
+    Vec3 v = R.block<3, 2>(0, 0) * D;
 
-    double a_mag = dp.norm();
+    double a_mag = ceres::sqrt(D(0) * D(0) + D(1) * D(1));
 
-    ppdp = std::cos(a_mag) * p + std::sin(a_mag) * (v / a_mag);
+    XpD = ceres::cos(a_mag) * X + ceres::sin(a_mag) * (v / a_mag);
 
     return true;
 }
 
 bool SphericalParameterization::ComputeJacobian(const double *x, double *jacobian) const {
+    Eigen::Map<const Vec3> X(x);
+    Eigen::Map<Eigen::Matrix<double, 3, 2, Eigen::RowMajor>> J(jacobian);
 
+    Vec2 axis;
+    axis << -X(1), X(0); //, 0.0;
+
+    double c = X(2);
+
+    Mat3 skew = Mat3::Zero();
+    skew(0, 2) = axis(1);
+    skew(1, 2) = -axis(0);
+    skew(2, 0) = -axis(1);
+    skew(2, 1) = axis(0);
+
+    Mat3 R = Mat3::Identity() + skew + (1.0 / (1.0 + c)) * skew * skew;
+
+    J = R.block<3, 2>(0,0);
+
+    return true;
 }
 
 }
-
---
+++ wave_optimization/tests/ceres/spherical_param_test.cpp
@@ -0,0 +1,66 @@
+#include <ceres/ceres.h>
+#include <ceres/gradient_checker.h>
+#include <Eigen/Core>
+#include <unsupported/Eigen/MatrixFunctions>
+
+#include "wave/optimization/ceres/local_params/spherical_parameterization.hpp"
+#include "wave/wave_test.hpp"
+#include "wave/utils/math.hpp"
+#include "wave/geometry/transformation.hpp"
+
+namespace {
+
+class TestSpherical : public ceres::SizedCostFunction<3, 3> {
+ private:
+    const double *const normal;
+
+ public:
+    virtual ~TestSpherical() {};
+
+    TestSpherical(const double *const normal) : normal(normal) {}
+
+    virtual bool Evaluate(double const *const *parameters, double *residuals, double **jacobians) const {
+        // Want to align the optimization parameter with the normal, error is a - b
+
+        residuals[0] = -normal[0] + parameters[0][0];
+        residuals[1] = -normal[1] + parameters[0][1];
+        residuals[2] = -normal[2] + parameters[0][2];
+
+        if (jacobians) {
+            Eigen::Map<Eigen::Matrix<double, 3, 3, Eigen::RowMajor>> J(jacobians[0]);
+            J.setIdentity();
+        }
+        return true;
+    }
+};
+
+}
+
+namespace wave {
+
+TEST(easy_cost_function, alignment) {
+    Vec3 normal;
+    normal << -1, 2, 4;
+    normal.normalize();
+
+    Vec3 initial;
+    initial.setRandom();
+    initial.normalize();
+
+    ceres::Problem problem;
+    ceres::CostFunction *cost = new TestSpherical(normal.data());
+    problem.AddResidualBlock(cost, nullptr, initial.data());
+
+    ceres::LocalParameterization *spherical = new SphericalParameterization();
+    problem.SetParameterization(initial.data(), spherical);
+
+    ceres::Solver::Options options;
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
+    std::cout << summary.FullReport();
+
+    EXPECT_LT((initial - normal).norm(), 1e-6);
+    EXPECT_NEAR(initial.norm(), 1.0, 1e-6);
+}
+
+}
